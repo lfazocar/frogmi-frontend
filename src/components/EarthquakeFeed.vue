0a001228aa65ee103ba1079953f813f01aed4b28
Felipe <lfazocar@users.noreply.github.com>
2024-04-11 03:23:11 -0400

    feat: create PostComment component
    Creates a comment for a feature in the database via POST request

--- src/components/EarthquakeFeed.vue
+++ src/components/EarthquakeFeed.vue
@@ -2,31 +2,67 @@
   <div>
     <h1>USGS Earthquake Feed</h1>
     <h2>All Earthquakes from the past 30 days</h2>
-    <ul v-if="feed">
+    <ul v-if="features">
       <li
-        v-for="earthquake in feed.data"
-        :key="earthquake.id"
+        v-for="feature in features"
+        :key="feature.id"
       >
-        {{ earthquake }}
+        <div>
+          <ul>
+            <li
+              v-for="(attribute, name) in feature.attributes"
+              :key="feature.attributes.external_id">
+              {{ name }}: {{ attribute }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <PostComment @createComment="postNewComment(feature.id, $event)" />
+        </div>
       </li>
     </ul>
+    <div>
+      <p>Page: {{ pagination.current_page }}</p>
+      <p>Features per page: {{ pagination.per_page }}</p>
+      <p>Total features: {{ pagination.total }}</p>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import PostComment from './PostComment.vue';
 
-const feed = ref({});
-
+const features = ref({});
+const pagination = ref({})
 onMounted(async () => {
   try {
     const res = await fetch('http://localhost:3000/api/features?page=1&per_page=2%27');
     if(!res.ok) {
       throw new Error('Failed to fetch data');
     }
-    feed.value = await res.json();
+    const feed = await res.json();
+    features.value = feed.data;
+    pagination.value = feed.pagination;
   } catch (err) {
     console.error('Error fetching feed data:', err);
   }
 });
+
+const postNewComment = async (feature_id, comment) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/features/${feature_id}/comments`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(comment)
+    });
+    if(!res.ok) {
+      throw new Error('Failed to post comment');
+    }
+  } catch (err) {
+    console.error('Error creating comment:', err);
+  }
+};
 </script>
---
+++ src/components/PostComment.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>Comment this feature</h3>
+  <form id="form" @submit.prevent="onSubmit">
+    <div>
+      <label for="text">Comment</label>
+      <input type="text" v-model="comment" placeholder="Your comment here...">
+    </div>
+    <button>Create comment</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const comment = ref('');
+const emit = defineEmits(['createComment']);
+
+const onSubmit = () => {
+  if(!comment.value) {
+    console.log('empty form');
+    return;
+  }
+  const newComment = {
+    body: comment.value
+  }
+  comment.value = '';
+  emit('createComment', newComment)
+}
+</script>
